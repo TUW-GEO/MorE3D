4eb5cfe0fa6d6303b0b92b02c43757856a426d35
rarav <100199712+rarav@users.noreply.github.com>
2023-01-12 15:22:25 +0100

    added license

--- run_levelsets.py
+++ run_levelsets.py
@@ -8,8 +8,6 @@ Main to run the Point-based level-set for the extraction of 3D entities
 # Dependencies:
     - sklearn
 
-MIT License
-
 When using this code please cite:
 
 @article{Arav.etal2022,
@@ -21,6 +19,31 @@ When using this code please cite:
   year={2022},
   publisher={Copernicus GmbH}
 }
+
+---------------------------------------------------------------------
+-                               MorE3D
+---------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright 2022, Photogrammetry Research Group, TU Wien
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-----------------------------------------------------------------------------
+
 '''
 
 import numpy as np
